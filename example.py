035f8b1cd680b8b1c94ea7c72678eacc00e2ff63
dalin27 <77298980+dalin27@users.noreply.github.com>
2022-07-16 08:02:56 +0200

    changed output filename

--- example.py
+++ example.py
@@ -7,17 +7,23 @@ from qiskit.utils import QuantumInstance
 
 # todo: generate strings of operators in the Hamiltonian using itertools product or combinations (for operator pool)
 
-# Initialize system parameters for Ising
+#----------------------------------------------------------------------------------------------------#
+# Choose system parameters for Ising
 num_qubits = 3  # number of qubits/spins
 coup = 0.25  # coupling constant
 field = 1.0  # field strength
+depth = 3  # depth of the circuit ansatz
+
+# Choose algorithm parameters
 dt = 0.05  # small time step
 n_steps = 40  # number of time steps
-
-# Algorithm parameters
 overlap_treshold = 0.99999  # threshold for the overlap (the overlap refers to Eq. 1 in the main Ref)
-depth = 3  # depth of the circuit
-dw = 0.01  # initial shift
+dw = 0.01  # initial shift of the variational parameters
+shots = 800  # number of times to execute Qiskit circuit
+opt = 'sgd'  # choose gradient optimizer: 'sgd', 'adam', 'momentum' (only 'sgd' implemented here)
+cost = 'local'  # choose which type of cost function to use: 'global', 'local'
+grad = 'param_shift'  # choose how to estimate the gradient on hardware: 'param_shift', 'spsa'
+#----------------------------------------------------------------------------------------------------#
 
 # Initialize the variational parameters and the associated circuit.
 # 0's as initial parameters imply that the circuit ansatz is initially the identity operator (C(0)=1)
@@ -34,12 +40,16 @@ print(f'Hamiltonian:\n{H}')
 shift = np.array([dw]*len(init_params))
 print(f'Initial shift:\n{shift}')
 
-# Qiskit Aer currently includes three high performance simulator backends. The two of interest:
-# QasmSimulator: Allows ideal and noisy multi-shot execution of qiskit circuits and returns counts or memory
-# StatevectorSimulator: Allows ideal single-shot execution of qiskit circuits and returns the final statevector of
-# the simulator after application
-shots = 800
-backend = Aer.get_backend('statevector_simulator')  # can replace to 'qasm_simulator'
+# Choose backend. Qiskit Aer currently includes three high performance simulator backends. The two of interest:
+# QasmSimulator: Allows ideal and noisy multi-shot execution of Qiskit circuits and returns counts or memory
+# StatevectorSimulator: Allows ideal single-shot execution of Qiskit circuits and returns the final statevector of
+# the simulator after application.
+if shots > 1:
+	backend = Aer.get_backend('qasm_simulator')
+	out_filename = 'data/tfim_nspins={}_j={}_h={}_shots={}.json'.format(num_qubits,coup,field,shots)
+elif shots == 1:
+	backend = Aer.get_backend('statevector_simulator')
+	out_filename = 'data/tfim_nspins={}_j={}_h={}_statevec.json'.format(num_qubits,coup,field)
 instance = QuantumInstance(backend=backend, shots=shots)
 
 obs = {}  # Initialize a dictionary to store the observables to measure
@@ -56,11 +66,6 @@ print('Observables:')
 for (name_op, pauli_str) in obs.items():
 	print(f'{name_op} --> {pauli_str}')
 
-# Define the necessary parameters to run the algorithm
-opt = 'sgd'  # choose gradient optimizer: 'sgd', 'adam', 'momentum' (only 'sgd' implemented here)
-cost = 'local'  # choose which type of cost function to use: 'global', 'local'
-grad = 'param_shift'  # choose how to estimate the gradient on hardware: 'param_shift', 'spsa'
-
 # Instantiate the pVQD class
 algo = pVQD(hamiltonian   = H,
 			ansatz        = hweff_ansatz,
@@ -75,7 +80,7 @@ algo.run(overlap_treshold,
 		 dt,
 		 n_steps,
 	     obs_dict      = obs,
-	     filename      = 'data/tfim_nspins={}_j={}_h={}_statevec.json'.format(num_qubits,coup,field),
+	     filename      = out_filename,
 	     max_iter      = 50,
 	     opt           = opt,
 	     cost_fun      = cost,
